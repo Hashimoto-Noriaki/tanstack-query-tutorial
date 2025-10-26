8541c1b4fb9200bd48df1322c9d779f8aab3e738
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-27 02:11:39 +0900

    queryClientをインスタンス化して、QueryClientProviderでComponentを囲む

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import './App.css'
 import Posts from './components/Posts'
 
+const queryClient = new QueryClient()//queryClientをインスタンス化
+
 function App() {
-  return <>{<Posts/>}</>
+  //QueryClientProviderでComponentを囲む
+  return( 
+    <QueryClientProvider client={queryClient}>
+      {<Posts/>}
+    </QueryClientProvider>
+  )
 }
 
 export default App
