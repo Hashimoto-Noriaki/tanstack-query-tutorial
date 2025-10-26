28ed71131900a0526098c7a6b94c5e1eff368a8a
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-27 02:33:35 +0900

    useQueryの実装をconstで定義

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,7 +1,26 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
 const Posts = () => {
-  return (
-    <div>Posts</div>
-  )
+    const { data, status, error, isFetching } = useQuery({
+        queryKey: ["posts"],
+        queryFn: async () => {
+            const { data } = await axios.get(
+                "https://jsonplaceholder.typicode.com/posts"
+            );
+
+            return data;
+        },
+    });
+
+    // console.log(data)
+
+    return (
+        <div>
+            <h1>投稿一覧</h1>
+            <div>{}</div>
+        </div>
+    )
 }
 
 export default Posts
