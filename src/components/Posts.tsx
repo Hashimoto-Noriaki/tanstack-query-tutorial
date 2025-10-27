17046ece1f7618f3c5fe7c2ab9f3691c52554079
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-27 22:59:26 +0900

    pendingの処理

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 
 const Posts = () => {
-    const { data } = useQuery({
+    const { data, status, error, isFetching } = useQuery({
         queryKey: ["posts"],
         queryFn: async () => {
             const { data } = await axios.get(
@@ -18,11 +18,19 @@ const Posts = () => {
     return (
         <div>
             <h1>投稿一覧</h1>
-            <div>{data.map((post)=> (
-                <p key={post.id}>
-                    <a href="/">{post.title}</a>
-                </p>
-            ))}</div>
+            <div>
+                {status === "pending" ? (
+                    <>読み込み中</>
+                ) : (
+                    <>
+                        {data.map((post)=> (
+                            <p key={post.id}>
+                                <a href="/">{post.title}</a>
+                            </p>
+                        ))}
+                    </>
+                )}
+            </div>
         </div>
     )
 }
